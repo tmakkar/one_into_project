48d589c4a4babe2f7ff02f28a8972efb7c177110
Tejveer Singh <tmakkar@academic.rrc.ca>
2025-03-20 08:04:22 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_17_190431) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_20_120733) do
   create_table "breeds", force: :cascade do |t|
     t.string "name"
     t.string "sub_breed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dog_breeds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dog_ownerships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "dog_breed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_breed_id"], name: "index_dog_ownerships_on_dog_breed_id"
+    t.index ["user_id"], name: "index_dog_ownerships_on_user_id"
   end
 
   create_table "images", force: :cascade do |t|
@@ -26,5 +48,35 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_17_190431) do
     t.index ["breed_id"], name: "index_images_on_breed_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_name"
+    t.string "email"
+    t.text "address"
+    t.datetime "order_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "dog_ownerships", "dog_breeds"
+  add_foreign_key "dog_ownerships", "users"
   add_foreign_key "images", "breeds"
+  add_foreign_key "products", "categories"
 end
